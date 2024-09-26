0b93fa0b719837fae8a5b0f512f3470b811fc298
Chris Runo <chris.runo@gmail.com>
2024-09-26 14:26:59 -0400

    Add viper for configuration management.

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
-/*
-Copyright © 2024 Chris Runo chris.runo@gmail.com
-*/
-
 package main
 
-import "CoreFlow/cmd"
+import (
+	"CoreFlow/cmd"
+	"CoreFlow/internal/database"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+}
 
 func main() {
+	initConfig()
+
+	dbConfig := database.Config{
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetInt("database.port"),
+		User:     viper.GetString("database.user"),
+		Password: viper.GetString("database.password"),
+		DBName:   viper.GetString("database.name"),
+	}
+
+	err := database.Connect(dbConfig)
+	if err != nil {
+		log.Fatalf("Could not connect to the database: %v", err)
+	}
+
+	defer func(DB *sql.DB) {
+		err := DB.Close()
+		if err != nil {
+			fmt.Printf("Could not close the database connection: %v", err)
+		}
+	}(database.DB)
+
 	cmd.Execute()
 }
